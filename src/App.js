0c5c44a193e9dcf3d3d14bb01e7a7862cc31c458
ismilearefin <arefinismile@gmail.com>
2023-01-25 01:17:41 +0600

    UseReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
+import ComplexCounter from './component/ComplexCounter';
+
+const initialState = 0;
+const reducer = (state, action)=>{
+  switch (action){
+    case 'increment':
+      return state + 1;
+      case 'decrement':
+        return state - 1;
+      default :
+      return state; 
+  }
+}
 
 function App() {
+   const [count, dispatch]  =  useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <h1>
         Hello Sobai
       </h1>
-      <p>
-        lorem Lorem Lorem
-      </p>
+      <div>
+        <div>Count = {count} </div>
+        <button type='button' onClick={()=> dispatch('increment')}>Increment</button>
+        <button type='button' onClick={()=> dispatch('decrement')}>Decrement</button>
+      </div>
+      <ComplexCounter></ComplexCounter>
     </div>
   );
 }
